e299456c129a8d3e2cf527e2a1df5d4c08fd6529
junyoung-sim <jysim.ny@gmail.com>
2024-05-03 10:26:07 -0400

    2024-05-03

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Progress Report: https://sites.google.com/icsd.k12.ny.us/comp-eng-wise/blog-entries
 
+8-bit Computer (as of May 3, 2024)
 ![alt text](https://github.com/junyoung-sim/8-bit/blob/main/res/IMG_8195.jpeg)
 
+Arduino Nano EEPROM Programmer
 ![alt text](https://github.com/junyoung-sim/8-bit/blob/main/res/IMG_8254.jpg)
--- converter/convert.cpp
+++ converter/convert.cpp
@@ -1,12 +1,56 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <map>
 
 int main(int argc, char *argv[])
 {
-    unsigned int sys0 = (int)argv[1];
-    unsigned int sys1 = (int)argv[2];
+    unsigned int sys0 = std::atoi(argv[1]);
+    unsigned int sys1 = std::atoi(argv[2]);
 
-    
+    // decimal to binary
+    if(sys0 == 10 && sys1 == 2) {
+        int decimal = std::atoi(argv[3]);
+        int binary[32], i = 32;
+        while(decimal) {
+            binary[--i] = decimal & 1;
+            decimal >>= 1;
+        }
+        for(; i < 32; i++)
+            std::cout << binary[i];
+        std::cout << "\n";
+    }
+
+    // binary to decimal
+    else if(sys0 == 2 && sys1 == 10) {
+        int binary = std::atoi(argv[3]);
+        int decimal = 0, power = 0;
+        while(binary) {
+            decimal += (binary % 10 ? 1 << power : 0);
+            binary /= 10;
+            power++;
+        }
+        std::cout << decimal << "\n";
+    }
+
+    // binary to hex
+    else if(sys0 == 2 && sys1 == 16) {
+        std::map<std::string, char> hex_dict;
+        hex_dict["0000"] = '0'; hex_dict["0001"] = '1'; hex_dict["0010"] = '2'; hex_dict["0011"] = '3';
+        hex_dict["0100"] = '4'; hex_dict["0101"] = '5'; hex_dict["0110"] = '6'; hex_dict["0111"] = '7';
+        hex_dict["1000"] = '8'; hex_dict["1001"] = '9'; hex_dict["1010"] = 'A'; hex_dict["1011"] = 'B';
+        hex_dict["1100"] = 'C'; hex_dict["1101"] = 'D'; hex_dict["1110"] = 'E'; hex_dict["1111"] = 'F';
+
+        std::string hex = "";
+        std::string binary = argv[3];
+        for(int i = 0; i < binary.length(); i += 4) {
+            std::string group = binary.substr(i, 4);
+            hex += hex_dict[group];
+        }
+        std::cout << hex << "\n";
+    }
+
+    else {}
 
     return 0;
 }
